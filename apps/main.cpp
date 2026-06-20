bb979ec6f191930af4da2fe70e56e17fe0565e2b
Hunger-Prevails <yinglun.liu.it.happens@gmail.com>
2026-06-21 01:05:27 +0200

    args

--- apps/main.cpp
+++ apps/main.cpp
@@ -5,13 +5,27 @@
 # include <memory>
 # include <Eigen/Dense>
 # include <nlohmann/json.hpp>
+# include <cxxopts.hpp>
+
+# include "calibrator.hpp"
 
 namespace fs = std::filesystem;
 
 using json = nlohmann::json;
 
 
-Eigen::Matrix3d load_intrinsics(const fs::path& filepath, const std::string& camera) {
+std::vector<fs::path> get_files_in_directory(const fs::path& directory) {
+    std::vector<fs::path> files;
+    for (const auto& entry : fs::directory_iterator(directory)) {
+        if (entry.is_regular_file()) {
+            files.push_back(entry.path());
+        }
+    }
+    return files;
+}
+
+
+Eigen::Matrix3d load_intrinsics(const fs::path& filepath) {
     std::ifstream file(filepath);
     if (!file.is_open()) {
         throw std::invalid_argument("Failed to open file: " + filepath.string());
@@ -21,17 +35,64 @@ Eigen::Matrix3d load_intrinsics(const fs::path& filepath, const std::string& cam
     file >> matrix_data;
     file.close();
 
+    if (!matrix_data.is_object()) {
+        throw std::invalid_argument("Invalid JSON format: expected an object");
+    }
+
+    if (!matrix_data.contains("K") || !matrix_data["K"].is_array()) {
+        throw std::invalid_argument("Invalid JSON format: expected 'K' to be an intrinsics array");
+    }
+
     Eigen::Matrix3d matrix;
 
     for (size_t i = 0; i < 3; ++i) {
         for (size_t j = 0; j < 3; ++j) {
-            matrix(i, j) = matrix_data[camera][i][j].get<double>();
+            matrix(i, j) = matrix_data["K"][i * 3 + j].get<double>();
         }
     }
     return matrix;
 }
 
-int main(int argc, char *argv[])
-{
+
+enum class Mode {Diamond, Cylinder};
+
+Mode parse_mode(const std::string& text) {
+    if (text == "Diamond") {
+        return Mode::Diamond;
+    }
+    if (text == "Cylinder") {
+        return Mode::Cylinder;
+    }
+
+    throw std::invalid_argument("invalid mode: " + text);
+}
+
+int main(int argc, char *argv[]) {
+    cxxopts::Options options("lidar-camera-calibration", "options to configure lidar-camera calibration pipeline");
+
+    options.add_options()("mode", "Calibration mode", cxxopts::value<std::string>());
+    options.add_options()("images", "Path to the image to process", cxxopts::value<fs::path>());
+    options.add_options()("point-clouds", "Path to the point cloud to process", cxxopts::value<fs::path>());
+    options.add_options()("intrinsics", "Path to the intrinsics file", cxxopts::value<fs::path>());
+    options.add_options()("write-path", "Path to write outputs to", cxxopts::value<fs::path>()->default_value("outputs"));
+
+    auto args = options.parse(argc, argv);
+
+    auto mode = parse_mode(args["mode"].as<std::string>());
+
+    std::unique_ptr<Calibrator> calibrator;
+
+    if (mode == Mode::Diamond) {
+        calibrator = std::make_unique<DiamondCalibrator>();
+    } else {
+        calibrator = std::make_unique<CylinderCalibrator>();
+    }
+
+    auto image_paths = get_files_in_directory(args["images"].as<fs::path>());
+    auto point_cloud_paths = get_files_in_directory(args["point-clouds"].as<fs::path>());
+
+    auto intrinsics = load_intrinsics(args["intrinsics"].as<fs::path>());
+
+    calibrator->calibrate(image_paths, point_cloud_paths, intrinsics);
     return 0;
 }
--- src/calibrator.cpp
+++ src/calibrator.cpp
@@ -1,9 +1,17 @@
 # include "calibrator.hpp"
+# include <pcl/point_types.h>
+# include <pcl/point_cloud.h>
+
+using PointT = pcl::PointXYZ;
+using CloudT = pcl::PointCloud<PointT>;
+using CloudPtr = CloudT::Ptr;
 
 void DiamondCalibrator::calibrate(std::vector<fs::path> image_paths, std::vector<fs::path> point_cloud_paths, Eigen::Matrix3d& intrinsics) {
+    std::cout << "To calibrate against diamond target..." << std::endl;
     return;
 }
 
 void CylinderCalibrator::calibrate(std::vector<fs::path> image_paths, std::vector<fs::path> point_cloud_paths, Eigen::Matrix3d& intrinsics) {
+    std::cout << "To calibrate against cylinder target..." << std::endl;
     return;
 }
